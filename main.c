f4cb92c61f2c198294580cdca27037bd56a48b17
lewisthangwa <113664066+lewisthangwa@users.noreply.github.com>
2022-11-27 00:11:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,84 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+void main(void)
 {
-    int code, qty;
-    float price, amt, totalAmt, cash, change, vat;
+    int gadget, quantity;
+    float price, amt, totalAmt, cash, vat, BAT, rate = 0.16;
     char addAnother;
     do {
          system("cls");
-         printf("===============\n");
-    printf("PRO GADGET SHOP\n");
-    printf("===============\n\n");
-    printf("[1] Laptop\t\tKSh 30000.00\n");
-    printf("[2] Joysticks\t\tKSh 1000.00\n");
-
-    printf("\nEnter Code\t: ");
-    scanf("%d", &code);
-    printf("\nEnter Quantity\t: ");
-    scanf("%d", &qty);
-
-    switch(code) {
-          case 1: price = 30000.00;
-                  break;
-          case 2: price = 1000.00;
-                  break;
-
-    }
-      amt = price * qty;
-      printf("\nAmount\t: % .2f", amt);
-       totalAmt = totalAmt + amt;
-       printf("\nTotal Amount\t: %.2f", totalAmt);
-       printf("\nAdd another order(y/n)?");
-       addAnother = getche ();
-    }while(addAnother == 'y' || addAnother == 'Y');
-
-    do{
-
-    printf("\nCash paid\t: ");
-    scanf("%f", &cash);
-
-    }while(cash < totalAmt);
-
-    printf("\nCash paid\t: ");
-    scanf("%f", &cash);
-
-    change = cash - totalAmt;
-    printf("\Change\t\t: %2f", change);
-    printf("\n\nThank You!");
-
     printf("===============\n");
     printf("PRO GADGET SHOP\n");
     printf("===============\n\n");
     printf("[1] Laptop\t\tKSh 30000.00\n");
     printf("[2] Joysticks\t\tKSh 1000.00\n");
 
-    printf("\nEnter Code\t: ");
-    scanf("%d", &code);
-    printf("\nEnter Quantity\t: ");
-    scanf("%d", &qty);
+    printf("what gadget do you want\n ");
+    scanf("%d", &gadget);
+    printf("how many gadgets:?\n ");
+    scanf("%d", &quantity);
 
-    switch(code) {
+    switch(gadget) {
           case 1: price = 30000.00;
                   break;
           case 2: price = 1000.00;
                   break;
 
     }
-      amt = price * qty;
-      printf("\nAmount\t: % .2f", amt);
+      amt = price * quantity;
+      printf("Amount: %.2f\n", amt);
 
-      printf("Please enter the VAT percentage:\n");
-      scanf("%f",&vat);
-      printf("Please enter the item value:\n");
-      scanf("%f",&price);
+       totalAmt = totalAmt + amt;
+       printf("Total Amount\t: %.2f\n", totalAmt);
 
-      float price_to_cost_conversion(price, vat)
-{
-float cost;
-cost=((1+(vat/100))*price);
-return cost;
-}
-printf("The total price is %f", price_to_cost_conversion);
+       printf("\nAdd another order(y/n)?\n");
+       addAnother = getche ();
+    }while(addAnother == 'y' || addAnother == 'Y');
+
+    do{
+
+    printf("\nCash paid\t:\n");
+    scanf("%f", &cash);
+
+    }while(cash < totalAmt);
+
+    vat = rate * cash;
+    printf("VAT: %.3f\n", vat);
+
+    BAT=cash - vat;
+    printf("change after tax: %.2f\n", BAT);
+
+return 0;
 }
